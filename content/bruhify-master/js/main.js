f0e38b864a09b9b489abd4333f517021d5af593e
TheTagOfHash <60450327+TheTagOfHash@users.noreply.github.com>
2020-01-30 20:32:34 -0500

    Update main.js

--- content/bruhify-master/js/main.js
+++ content/bruhify-master/js/main.js
@@ -30,6 +30,7 @@ const app = new Vue({
                 faideye: false,
 		algebruh_moment: false,
 		carson: false
+		testing: false
             }
         },
         upgrade: {
@@ -147,6 +148,8 @@ const app = new Vue({
                 this.multiplier += 10;
 	    } else if (promo_code('algebruh moment')) {
 		this.multiplier += 15;
+	    } else if (promo_code('testing')) {
+		this.bruhs += 10000;
 	    } else if (promo_code('jr7tnuycex5gto6udk78juyqzy32lms2rs7uduecdzk9i88kl3zx95kbj8hk4aqx4dj72xr2ezrirgwhlw5nthl59qnvfvo7epvzftuh7zlgo2f44enjbfze0xwmg6qmdewokjgi70r65o5k54dj5gxkc75tqphcaht4nbvm0lcw8djiye2qnmlnoz8zswa4arf09qidpxs9s7ctng6yoe6dd66agy43sxd9nlci1hctuzkkswyefl1663wxp7rk1jshrf04kqgg7ud1fkjakwe0scha643enybc074vojx935vispqlh9692px0tzcheg6s1q92f3ri4unldz2b9yj8igo3a5ki9s7tccjlcqbcj0f1sfjmhvaoje38sg1r91iyaydw9mwetbuw1sc968q0t489hifpnhrdp7fq91a9rfsu6kchpmr75yht1tolvt1qd07ga3f4mmwp6grc3ktojjykle1mihmu4adjglrc2sb884ka6rzs6j9yxd065wbnwx6wyubjomf70hw3ylnuo5qyvvxhhm715qfqbx4d2lhv71za19dwt8rsh')) {
 		this.multiplier -= 100;
             } else {
